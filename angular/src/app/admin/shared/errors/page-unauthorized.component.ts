591bde8f3751c7e19f9087d309249cb1f721bc85
Romell Jaramillo <romelljaramillo@gmail.com>
2024-02-08 18:24:54 +0100

    add changes page errors

--- angular/src/app/admin/shared/errors/page-unauthorized.component.ts
+++ angular/src/app/admin/shared/errors/page-unauthorized.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-pagenofound',
+  selector: 'app-page-unauthorized',
   standalone: true,
   imports: [RouterModule],
   template: `
